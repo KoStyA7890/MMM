b2b0b08a5340ac79a247751b18e7b0cadc03e3d3
USER <kostya7893@gmail.com>
2022-10-23 14:26:40 +0300

    3

--- main.py
+++ main.py
@@ -22,3 +22,71 @@ choice == "r":
  elif str.lower(choice) == "s" and computer_
 choice == "p":
  result["player"] += 1
+import random
+def game(choice, result):
+ print("")
+ print("=====Start Game rock, paper,
+ scissors=====")
+ computer_choice = random.choice("rps")
+ print("--------------------------------")
+ print("Your select – ",
+ str.capitalize(choice))
+ print("Computer select —",
+ str.capitalize(computer_choice))
+ if str.lower(choice) == computer_choice:
+ print("Result of game – Draw")
+ print("Score,Computer", result
+ ["computer"], "—", result["player"],
+ "Player")
+ elif str.lower(choice) == "r" and computer_
+choice == "p":
+ result["computer"]+=1
+ print("------Computer Wins------")
+ print("Score,Computer", result
+ ["computer"],"—", result["player"],
+ "Player")
+ elif str.lower(choice) == "r" and computer_
+ choice == "s":
+ result["player"] += 1
+ print("------Player Wins------")
+ print("Score,Computer", result
+ ["computer"], "—", result["player"],
+       "Player")
+ elif str.lower(choice) == "p" and computer_
+ choice == "s":
+ result["computer"] += 1
+ print("------Computer Wins------")
+ print("Score,Computer", result
+ ["computer"], "—", result["player"],
+       "Player")
+ elif str.lower(choice) == "p" and computer_
+ choice == "r":
+ result["player"] += 1
+ print("------Player Wins------")
+ print("Score,Computer", result
+ ["computer"], "—", result["player"],
+       "Player")
+ elif str.lower(choice) == "s" and computer_
+ choice == "r":
+ result["computer"] += 1
+ print("------Computer Wins------")
+ print("Score,Computer", result
+ ["computer"], "—", result["player"],
+       "Player")
+ elif str.lower(choice) == "s" and computer_
+ choice == "p":
+ result["player"] += 1
+ print("------Player Wins------")
+ print("Score,Computer", result
+ ["computer"], "—", result["player"],
+       "Player")
+ result = {"computer": 0, "player": 0}
+ choise = input("Select R / P / S – ")
+game(choice=choise, result=result)
+ import random
+ def game(choice, result):
+  print("")
+  print("=====Start Game rock, paper,
+  scissors == == = ")
+  computer_choice = random.choice("rps")
+  print("--------------------------------")
