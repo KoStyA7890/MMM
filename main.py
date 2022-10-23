690bf2ee050be1dde73f6160f6b0c6ab9251132a
USER <kostya7893@gmail.com>
2022-10-23 14:36:24 +0300

    1.0

--- main.py
+++ main.py
@@ -1,92 +1,10 @@
 import random
-
-def game(choice, result):
- computer_choice = random.choice("rps")
- if str.lower(choice) == computer_choice:
- print("Draw")
- elif str.lower(choice) == "r" and computer_
- choice == "p":
- result["computer"]+=1
- elif str.lower(choice) == "r" and computer_
- choice == "s":
- result["player"]+=1
- elif str.lower(choice) == "p" and computer_
- choice == "s":
- result["computer"] += 1
- elif str.lower(choice) == "p" and computer_
- choice == "r":
- result["player"] += 1
- elif str.lower(choice) == "s" and computer_
-choice == "r":
- result["computer"] += 1
- elif str.lower(choice) == "s" and computer_
-choice == "p":
- result["player"] += 1
-import random
 def game(choice, result):
  print("")
- print("=====Start Game rock, paper,
- scissors=====")
+ print("=====Start Game rock, paper,scissors=====")
  computer_choice = random.choice("rps")
  print("--------------------------------")
  print("Your select – ",
- str.capitalize(choice))
+       str.capitalize(choice))
  print("Computer select —",
- str.capitalize(computer_choice))
- if str.lower(choice) == computer_choice:
- print("Result of game – Draw")
- print("Score,Computer", result
- ["computer"], "—", result["player"],
- "Player")
- elif str.lower(choice) == "r" and computer_
-choice == "p":
- result["computer"]+=1
- print("------Computer Wins------")
- print("Score,Computer", result
- ["computer"],"—", result["player"],
- "Player")
- elif str.lower(choice) == "r" and computer_
- choice == "s":
- result["player"] += 1
- print("------Player Wins------")
- print("Score,Computer", result
- ["computer"], "—", result["player"],
-       "Player")
- elif str.lower(choice) == "p" and computer_
- choice == "s":
- result["computer"] += 1
- print("------Computer Wins------")
- print("Score,Computer", result
- ["computer"], "—", result["player"],
-       "Player")
- elif str.lower(choice) == "p" and computer_
- choice == "r":
- result["player"] += 1
- print("------Player Wins------")
- print("Score,Computer", result
- ["computer"], "—", result["player"],
-       "Player")
- elif str.lower(choice) == "s" and computer_
- choice == "r":
- result["computer"] += 1
- print("------Computer Wins------")
- print("Score,Computer", result
- ["computer"], "—", result["player"],
-       "Player")
- elif str.lower(choice) == "s" and computer_
- choice == "p":
- result["player"] += 1
- print("------Player Wins------")
- print("Score,Computer", result
- ["computer"], "—", result["player"],
-       "Player")
- result = {"computer": 0, "player": 0}
- choise = input("Select R / P / S – ")
-game(choice=choise, result=result)
- import random
- def game(choice, result):
-  print("")
-  print("=====Start Game rock, paper,
-  scissors == == = ")
-  computer_choice = random.choice("rps")
-  print("--------------------------------")
+       str.capitalize(computer_choice))
