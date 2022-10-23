140aba74fd8b24784a3eef1d2af2b48c81e65b3f
USER <kostya7893@gmail.com>
2022-10-23 14:23:20 +0300

    2

--- main.py
+++ main.py
@@ -2,3 +2,23 @@ import random
 
 def game(choice, result):
  computer_choice = random.choice("rps")
+ if str.lower(choice) == computer_choice:
+ print("Draw")
+ elif str.lower(choice) == "r" and computer_
+ choice == "p":
+ result["computer"]+=1
+ elif str.lower(choice) == "r" and computer_
+ choice == "s":
+ result["player"]+=1
+ elif str.lower(choice) == "p" and computer_
+ choice == "s":
+ result["computer"] += 1
+ elif str.lower(choice) == "p" and computer_
+ choice == "r":
+ result["player"] += 1
+ elif str.lower(choice) == "s" and computer_
+choice == "r":
+ result["computer"] += 1
+ elif str.lower(choice) == "s" and computer_
+choice == "p":
+ result["player"] += 1
